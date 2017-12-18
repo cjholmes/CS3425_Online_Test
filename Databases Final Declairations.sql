7f8395fdecf110268759fc2cc7ce34432c80c7cc
Chris Holmes <cbdh2024@gmail.com>
2017-12-17 19:45:49 -0500

    More declairation changes

--- Databases Final Declairations.sql
+++ Databases Final Declairations.sql
@@ -22,25 +22,48 @@ CREATE TABLE grades(
 );
 
 CREATE TABLE exams (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(30),
   total INT
 );
 
 CREATE TABLE questions (
-  id INT PRIMARY KEY,
-  exam INT,
-  question VARCHAR(144),
+  questionnumber INT,
+  examname VARCHAR(30) REFERENCES exams(name),
+  question VARCHAR(511),
   num_options INT,
-  value INT,
-  FOREIGN KEY (exam) REFERENCES exams(id)
+  pointVal INT
 );
 
 CREATE TABLE options (
-  id INT PRIMARY KEY,
-  question INT,
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  examname VARCHAR(30) NOT NULL REFERENCES exams(name),
+  questionnum INT NOT NULL REFERENCES questions(questionnumber),
   text VARCHAR(144),
-  correct BOOLEAN,
-  FOREIGN KEY (question) REFERENCES questions(id)
+  correct BOOLEAN
 );
 
+CREATE PROCEDURE newStudent(IN usern VARCHAR(30), IN pass VARCHAR(30),
+  IN idnum INT, IN nam VARCHAR(50), IN maj VARCHAR(50))
+  BEGIN
+      INSERT INTO users VALUE (usern, pass, FALSE );
+      INSERT INTO student VALUES (usern, idnum, nam, maj);
+  END//
+
+CREATE PROCEDURE newExam (IN title VARCHAR(30), IN totalPoints INT)
+  BEGIN
+    INSERT INTO exams(name, total) VALUES (title, totalPoints);
+  END;
+
+CREATE PROCEDURE newQuestion(IN examName VARCHAR(30), IN questionNum INT,
+  IN questionText VARCHAR(511), IN numOptions INT, IN qval INT)
+  BEGIN
+    INSERT INTO questions VALUES (questionNum, examName, questionText, numOptions, qval);
+  END;
+
+CREATE PROCEDURE newOption(IN examName VARCHAR(30), IN questionNum INT, IN oText VARCHAR(144),
+  IN cor INT)
+  BEGIN
+    INSERT INTO options(examname, questionnum, text, correct) VALUES (examName, questionNum,
+    oText, cor);
+  END;
